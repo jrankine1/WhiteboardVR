7b0fdb21027451371cb17f26edccc5e14d774b13
jrankine1 <85931070+jrankine1@users.noreply.github.com>
2022-11-10 22:58:44 +1100

    0.1
    Started adding the hands

---
+++ Assets/Scripts/HandPresence.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class HandPresence : MonoBehaviour
+{
+    public InputDeviceCharacteristics controllerCharacter;
+    public GameObject handModelPrefab;
+    private InputDevice targetDevice;
+    private GameObject spawnedHandModel;
+    void Start()
+    {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacter, devices);
+        if(devices.Count > 0)
+        {
+            targetDevice = devices(0);
+        }
+        spawnedHandModel = Instantiate(handModelPrefab, transform);
+        handAnimator = spawnedHandModel.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
