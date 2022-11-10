51d373069fd2ebeadb5c73210637402cfe70fb3a
jrankine1 <85931070+jrankine1@users.noreply.github.com>
2022-11-10 23:20:04 +1100

    Finished Hand Functionality
    Finished the hands

--- Assets/Scripts/HandPresence.cs
+++ Assets/Scripts/HandPresence.cs
@@ -7,23 +7,51 @@ public class HandPresence : MonoBehaviour
 {
     public InputDeviceCharacteristics controllerCharacter;
     public GameObject handModelPrefab;
+
     private InputDevice targetDevice;
     private GameObject spawnedHandModel;
+
+    public Animator handAnimator;
+
     void Start()
     {
         List<InputDevice> devices = new List<InputDevice>();
         InputDevices.GetDevicesWithCharacteristics(controllerCharacter, devices);
         if(devices.Count > 0)
         {
-            targetDevice = devices(0);
+            targetDevice = devices[0];
         }
         spawnedHandModel = Instantiate(handModelPrefab, transform);
         handAnimator = spawnedHandModel.GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateHandAnimation();
     }
+
+    void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+
 }
